4dfc8f4a479a6b305a3b60e5519f14396a5fb3e6
ShawnWong1221 <540942667@qq.com>
2019-05-21 22:30:59 +0800

    更新了UserDao

---
+++ O2O/o2o/Dao/UserDao.cs
@@ -0,0 +1,42 @@
+﻿using Dao.Utils;
+using o2o.entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    public class UserDao
+    {
+       
+        public User queryUserById(int id)
+        {
+            User user = new User();
+            String sql = "SELECT * FROM tb_user WHERE id = @id and username = @username";
+            SqlCommand cmd = DbUtil.getCommand(sql);
+            cmd.Parameters.Add(new SqlParameter("@id",id));
+            SqlDataReader sdr =cmd.ExecuteReader();
+            user.Id = sdr.GetInt32(1);
+            user.Username = sdr.GetString(2);
+            DbUtil.close();
+            return user;
+
+        }
+        public Boolean updateUser(User user)
+        {
+            String sql = "UPDATE tb_user SET username = @username,password=@password";
+            SqlCommand cmd = DbUtil.getCommand(sql);
+            cmd.Parameters.Add(new SqlParameter("@username", user.Username));
+            cmd.Parameters.Add(new SqlParameter("@password",user.Password));
+            int i = cmd.ExecuteNonQuery();
+            if (i > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ O2O/o2o/Dao/Utils/DbUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Utils
+{
+    class DbUtil
+    {
+        private static SqlConnection conn;
+        public static SqlCommand getCommand(String sql)
+        {
+            String connStr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            conn = new SqlConnection(connStr);
+            SqlCommand myCmd = new SqlCommand(sql, conn);
+            return myCmd;
+        }
+        public static void close()
+        {
+            conn.Close();
+        }
+    }
+}
