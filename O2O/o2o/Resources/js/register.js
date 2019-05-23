c918bb44ec21ded9d8468d44e1fa2760b661f4b7
NoCortY <nocorty@163.com>
2019-05-23 16:46:54 +0800

    JS实现登陆验证

--- O2O/o2o/Resources/js/register.js
+++ O2O/o2o/Resources/js/register.js
@@ -1,14 +1,25 @@
 ﻿$(function () {
     var url = "../../Controller/register.ashx";
     $("#submit").click(function () {
-        $.ajax({
-            type:'POST',
-            url: url,
-            data: $('#registerform').serialize(),
-            dataType:'json',
-            success: function (data) {
-                alert(data.username);
-            }
-        });
+        if ($("#InputEmail").val() == "") {
+            $("#alertemail").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;邮箱不能为空");
+        }
+        if ($("#InputPassword").val() == "") {
+            $("#alertpassword").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;密码不能为空");
+        }
+        if ($("#InputNickname").val() == "") {
+            $("#alertnickname").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;昵称不能为空");
+        }
+        if (($("#InputEmail").val() != "") && ($("#InputPassword").val() == "") && ($("#InputNickname").val() == "")) {
+            $.ajax({
+                type:'POST',
+                url: url,
+                data: $('#registerform').serialize(),
+                dataType:'json',
+                success: function (data) {
+                    alert(data.username);
+                }
+            });
+        }
     });
 });
